8739d21a52e4d4be6cda94d1bc9f5a56e6807116
marcin.sowula <marcin.sowula@gmail.com>
2016-10-16 16:46:50 +0200

    Add Prism and Autofac

---
+++ Finanse/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Finanse
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
